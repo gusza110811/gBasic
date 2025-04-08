d9dc62af6e9b61ed220f5ff5710f122494c094c6
gusza110811 <100755292+gusza110811@users.noreply.github.com>
2025-04-08 14:49:08 +0700

    second commit

--- gbasic.py
+++ gbasic.py
@@ -1,15 +1,8 @@
 import os
 import sys
-import json
 
 class memory:
-    value = {"line'":0}
-    drive = {}
-
-def loaddrive():
-    global drive
-    with open(memory.value["curdrive'"]+".json",'r') as fdrive:
-        memory.drive = json.load(fdrive)
+    value = {"line":0}
 
 def strtobool(string:str):
     return not (string.lower()=="false")
@@ -29,9 +22,9 @@ class commands:
         return
     
     def dump(text):
+        length = max(map(len, memory.value))
         for idx, item in enumerate(memory.value):
-            if item.endswith("'"): continue
-            print(f"{item} : {memory.value[item]}")
+            print(f"{item.ljust(length," ")} : {memory.value[item]}")
     
     def set(text:list[str]):
         key = text[0]
@@ -53,146 +46,170 @@ class commands:
     
     def clean(text:list[str]):
         os.system("cls")
-    
-    def call(text:list[str]):
-        loaddrive()
-        name = text[0]
-        script = memory.drive[name]
-        line = memory.value["line'"]
-
-        execute({script})
-
-        memory.value["line'"] = line
 
     def cmd(text:list[str]):
         memory.value["mode'"] = "cmd"
-
-
-def parse(text:str, mem=None):
-    if mem:
-        memory.value = mem
-
-    lines = text.split("\n")
-
-    chunks:list[str] = []
-    # parse each line
-    for line in lines:
-        if line.startswith("#"):
-            chunks.append("comment")
-            continue
-
-        lowchunks = line.split()
-
-        chunkmerge = ""
-        for chunk in lowchunks:
-
-            if chunk.startswith("#"):
-                break
-            
-            if chunk.startswith('"') or chunk.startswith("'"):
-                if chunk.endswith('"') or chunk.endswith("'"):
-                    chunks.append(chunk[1:-1])
-                else:
-                    chunkmerge = chunk[1:]
-            
-            elif chunk.endswith('"') or chunk.endswith("'"):
-                if chunkmerge != "":
-                    chunkmerge = chunkmerge + " " + chunk[:-1]
-                else:
-                    chunkmerge = chunk[:-1]
-                chunks.append(chunkmerge)
-                chunkmerge = ""
-            elif chunkmerge != "":
-                chunkmerge = chunkmerge + " " + chunk
-
-            else:
-                chunks.append(chunk)
     
-    # replace variables with their value
-    for index, chunk in enumerate(chunks):
-        if chunk.startswith("$"):
-            try:
-                chunks[index] = memory.value[chunk[1:]]
-            except KeyError:
-                return f"Variable '{chunk[1:]}' doesn't exist."
-    
-    # do calculations (no specific orders)
-    done = False
-    while not done:
-        for idx, chunk in enumerate(chunks):
-            # algebra
-            try:
-                if chunk == "+":
-                    chunks[idx] = str(float(chunks[idx-1]) + float(chunks[idx+1]))
-                    chunks.pop(idx-1)
-                    chunks.pop(idx)
+    def gotoif(text:list[str]):
+        condition = text[0]
+        line = text[1]
+
+        if strtobool(condition): memory.value["line"] = int(line)
+
+        return
+
+class parser:
+    def parse(text:str, mem=None):
+        if mem:
+            memory.value = mem
+
+        lines = text.split("\n")
+
+        chunks:list[list[str]] = []
+        # parse each line
+        for linenum, line in enumerate(lines):
+            linechunks = []
+            if line.startswith("#"):
+                chunks.append(["comment"])
+                continue
+            if line == "":
+                chunks.append(["comment"])
+                continue
+
+            lowchunks = line.split()
+
+            chunkmerge = ""
+            for idx, chunk in enumerate(lowchunks):
+
+                if chunk.startswith("#"):
                     break
-                elif chunk == "-":
-                    chunks[idx] = str(float(chunks[idx-1]) - float(chunks[idx+1]))
-                    chunks.pop(idx-1)
-                    chunks.pop(idx)
-                    break
-                elif chunk == "*":
-                    chunks[idx] = str(float(chunks[idx-1]) * float(chunks[idx+1]))
-                    chunks.pop(idx-1)
-                    chunks.pop(idx)
-                    break
-                elif chunk == "**":
-                    chunks[idx] = str(float(chunks[idx-1]) ** float(chunks[idx+1]))
-                    chunks.pop(idx-1)
-                    chunks.pop(idx)
-                    break
-                elif chunk == "/":
-                    chunks[idx] = str(float(chunks[idx-1]) / float(chunks[idx+1]))
-                    chunks.pop(idx-1)
-                    chunks.pop(idx)
-                    break
-            except ValueError:
-                return "Type error: You must use Number"
-            # conditional
-            if chunk == "==":
+                
+                if idx == 0:
+                    if chunk.startswith("?"):
+                        linechunks.append("gotoif")
+                        chunk = chunk[1:]
+                    elif chunk == "if":
+                        linechunks.append("gotoif")
+                        continue
+                    if chunk.startswith(":"):
+                        memory.value[chunk[1:]] = linenum
+                        linechunks.append("comment")
+                        continue
+                
+                if chunk.startswith('"') or chunk.startswith("'"):
+                    if chunk.endswith('"') or chunk.endswith("'"):
+                        linechunks.append(chunk[1:-1])
+                    else:
+                        chunkmerge = chunk[1:]
+                
+                elif chunk.endswith('"') or chunk.endswith("'"):
+                    if chunkmerge != "":
+                        chunkmerge = chunkmerge + " " + chunk[:-1]
+                    else:
+                        chunkmerge = chunk[:-1]
+                    linechunks.append(chunkmerge)
+                    chunkmerge = ""
+                elif chunkmerge != "":
+                    chunkmerge = chunkmerge + " " + chunk
+
+                else:
+                    linechunks.append(chunk)
+            chunks.append(linechunks)
+
+        return chunks
+
+    def variableparser(linechunks:list):
+
+        if linechunks[0] == "comment": return linechunks
+
+        linechunks = linechunks.copy()
+
+        # replace variables with their value
+        for index, chunk in enumerate(linechunks):
+            if chunk.startswith("$"):
+                linechunks[index] = memory.value[linechunks[index][1:]]
+        return linechunks
+
+    def calculationparser(chunks:list):
+        if chunks[0] == "comment": return chunks
+
+        chunks = chunks.copy()
+
+        # do calculations (no specific orders)
+        done = False
+        while not done:
+            for idx, chunk in enumerate(chunks):
+                # algebra
                 try:
-                    chunks[idx] = str(float(chunks[idx-1]) == float(chunks[idx+1]))
+                    if chunk == "+":
+                        chunks[idx] = str(float(chunks[idx-1]) + float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                    elif chunk == "-":
+                        chunks[idx] = str(float(chunks[idx-1]) - float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                    elif chunk == "*":
+                        chunks[idx] = str(float(chunks[idx-1]) * float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                    elif chunk == "**":
+                        chunks[idx] = str(float(chunks[idx-1]) ** float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                    elif chunk == "/":
+                        chunks[idx] = str(float(chunks[idx-1]) / float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
                 except ValueError:
-                    chunks[idx] = str(chunks[idx-1] == chunks[idx+1])
-                chunks.pop(idx-1)
-                chunks.pop(idx)
-                break
-            try:
-                if chunk == "<":
-                    chunks[idx] = str(float(chunks[idx-1]) < float(chunks[idx+1]))
+                    return "Type error: You must use Number"
+                
+                # conditional
+                if chunk == "==":
+                    try:
+                        chunks[idx] = str(float(chunks[idx-1]) == float(chunks[idx+1]))
+                    except ValueError:
+                        chunks[idx] = str(chunks[idx-1] == chunks[idx+1])
                     chunks.pop(idx-1)
                     chunks.pop(idx)
                     break
-                elif chunk == ">":
-                    chunks[idx] = str(float(chunks[idx-1]) > float(chunks[idx+1]))
+                try:
+                    if chunk == "<":
+                        chunks[idx] = str(float(chunks[idx-1]) < float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                    elif chunk == ">":
+                        chunks[idx] = str(float(chunks[idx-1]) > float(chunks[idx+1]))
+                        chunks.pop(idx-1)
+                        chunks.pop(idx)
+                        break
+                except ValueError:
+                    return "Type error: You must use Number"
+                
+                # boolean algebra
+                if chunk == "||":
+                    chunks[idx] = str(strtobool(chunks[idx-1]) | strtobool(chunks[idx+1]))
                     chunks.pop(idx-1)
                     chunks.pop(idx)
                     break
-            except ValueError:
-                return "Type error: You must use Number"
-            # boolean algebra
-            if chunk == "||":
-                chunks[idx] = str(strtobool(chunks[idx-1]) | strtobool(chunks[idx+1]))
-                chunks.pop(idx-1)
-                chunks.pop(idx)
-                break
-            elif chunk == "&&":
-                chunks[idx] = str(strtobool(chunks[idx-1]) & strtobool(chunks[idx+1]))
-                chunks.pop(idx-1)
-                chunks.pop(idx)
-                break
-            elif chunk == "-!":
-                chunks[idx] = str(not strtobool(chunks[idx+1]))
-                chunks.pop(idx+1)
-                break
-        else:
-            done = True
-    
-    if not chunks:
-        chunks.append("comment")
-
-    return chunks
+                elif chunk == "&&":
+                    chunks[idx] = str(strtobool(chunks[idx-1]) & strtobool(chunks[idx+1]))
+                    chunks.pop(idx-1)
+                    chunks.pop(idx)
+                    break
+                elif chunk == "~~":
+                    chunks[idx] = str(not strtobool(chunks[idx+1]))
+                    chunks.pop(idx+1)
+                    break
+            else:
+                done = True
+        return chunks
 
 def executeline(line:list[str], mem:list[str]=None):
 
@@ -208,30 +225,29 @@ def executeline(line:list[str], mem:list[str]=None):
         except IndexError:
             return memory.value, "Not enough parameters!"
     except AttributeError:
-        return memory.value, "Unrecognized command: {line[0]}"
+        return memory.value, f"Unrecognized command: {line[0]}"
     except SyntaxError:
-        return memory.value, "Unrecognized command: {line[0]}"
+        return memory.value, f"Unrecognized command: {line[0]}"
 
     return memory.value, None
 
-def execute(lines:list[str]|str, mem:list[str]=None):
-    if type(lines) is str: lines = lines.split("\n")
+def execute(lines:str, mem:list[str]=None):
 
     if mem: memory.value = mem
 
-    memory.value["line'"] = 0
+    memory.value["line"] = 0
+    parsed = parser.parse(lines)
 
-    while memory.value["line'"] < len(lines):
-        mem, error = executeline(parse(lines[memory.value["line'"]]))
+    while memory.value["line"] < len(lines.split("\n")):
+        mem, error = executeline(parser.calculationparser(parser.variableparser(parsed[memory.value["line"]])))
         if error:
-            print(f"At line {memory.value["line'"]} `{lines[memory.value["line'"]]}`:")
+            print(f"At line {memory.value["line"]+1} `{lines[memory.value["line"]]}`:")
             print(f"    {error}")
             return memory.value
-        memory.value["line'"] += 1
+        memory.value["line"] += 1
     
     return memory.value
 
-testmode = True
 if __name__ == "__main__":
     try:
         filename = sys.argv[1]
